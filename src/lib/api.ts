ad0769280f72a21524e3898df695b3bb60ed550b
varshiniv1 <varshini.venkat1@gmail.com>
2025-06-15 01:24:32 -0400

    updated the api path

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://your-backend.onrender.com/api',
+  baseURL: 'https://vendor-api-gvc7.onrender.com/api',
 
 });
 
